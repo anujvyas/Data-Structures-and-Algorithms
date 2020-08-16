694e4f4bb0dc05c68fcb31569dbd34c826c5f1c4
Anuj Vyas <54404551+anujvyas@users.noreply.github.com>
2020-08-16 20:21:58 +0530

    Add files via upload

--- Data Structures/3. Linked List/singly-linked-list.py
+++ Data Structures/3. Linked List/singly-linked-list.py
@@ -1,5 +1,15 @@
 # Data Structure: LinkedList
-# Methods: append, prepend, insert_node_after, traverse
+# Methods: 	1. traverse
+# 			2. insertion:
+# 				• append_node
+# 				• prepend_node
+# 				• insert_node_after
+# 			3. deletion:
+# 				• delete_node
+# 				• delete_node_at_position
+
+
+
 
 class Node:
 	def __init__(self, data):
@@ -11,7 +21,23 @@ class LinkedList:
 	def __init__(self):
 		self.head = None
 
-	def append(self, data):
+
+	def traverse(self):
+		# If linkedlist is empty
+		if self.head == None:
+			print('Cannot traverse as linkedlist is empty!')
+
+		# If linkedlist has nodes
+		temp = self.head
+		# Print values of all nodes except last
+		while temp.next != None:
+			print('{} -> '.format(temp.data), end='')
+			temp = temp.next
+		# Print value of last node
+		print('{} -> NULL'.format(temp.data))
+
+	# -------------- Insertion --------------
+	def append_node(self, data):
 		new_node = Node(data)
 
 		# If linkedlist is empty
@@ -25,7 +51,7 @@ class LinkedList:
 			temp = temp.next
 		temp.next = new_node
 
-	def prepend(self, data):
+	def prepend_node(self, data):
 		new_node = Node(data)
 
 		# If linkedlist is empty or not empty
@@ -60,36 +86,110 @@ class LinkedList:
 		else:
 			print('Cannot insert node after "{}" as value not found in linkedlist!'.format(prev_data))
 
-	def traverse(self):
+
+
+	# -------------- Deletion --------------
+	def delete_node(self, data):
 		# If linkedlist is empty
 		if self.head == None:
-			print('Cannot traverse as linkedlist is empty!')
+			print('Cannot delete node "{}" as linkedlist is empty!'.format(data))
+			return -1
 
-		# If linkedlist has nodes
+		# Search for matching value
 		temp = self.head
-		# Print values of all nodes except last
+		behind_temp = self.head
 		while temp.next != None:
-			print('{} -> '.format(temp.data), end='')
+			# If found the data
+			if temp.data == data:
+				
+				# If data is at head
+				if temp == self.head:
+					self.head = self.head.next
+
+				# If data is not at head
+				else:
+					behind_temp.next = temp.next
+				return -1
+			
+			# If first iteration
+			if temp != behind_temp:
+				behind_temp = temp
+
 			temp = temp.next
-		# Print value of last node
-		print('{} -> NULL'.format(temp.data))
 
+		# If last node has matching value
+		if temp.data == data:
+			behind_temp.next = temp.next
+			return -1
+		
+		# If value not found in linkedlist
+		else:
+			print('Cannot delete node "{}" as value not found in linkedlist!'.format(data))
 
+	def delete_node_at_position(self, position):
+		# If linkedlist is empty
+		if self.head == None:
+			print('Cannot delete node at position "{}" as linkedlist is empty!'.format(position))
+			return -1
+
+		if position == 0:
+			self.head = self.head.next
+			return -1
+
+		temp = self.head
+		current_position = 0
+		flag = -1
+
+		while temp.next != None:
+			if position == current_position:
+				flag = 1
+				break 
+
+			temp = temp.next
+			current_position += 1
+
+		# If found element or last node is at the given position
+		if flag == 1 or position == current_position:
+			temp1 = self.head
+
+			# Find temp
+			while temp1.next != temp:
+				temp1 = temp1.next
+
+			# Skip the temp node
+			temp1.next = temp1.next.next
+			return -1
+
+		# If position not found in linkedlist
+		else:
+			print('Cannot delete node at position "{}" as position > len(linkedlist)'.format(position))
 
 
 
 if __name__ == '__main__':
 	l = LinkedList()
 
-	l.append("C")
-	l.append("D")
+	# -------------- Insertion --------------
+	l.append_node("C")
+	l.append_node("D")
 	l.traverse()
 
-	l.prepend("B")
-	l.prepend("A")
+	l.prepend_node("B")
+	l.prepend_node("A")
 	l.traverse()
 
 	l.insert_node_after("D", "E")
 	l.traverse()
 
 	l.insert_node_after("Z", "E")
+	l.traverse()
+
+	# -------------- Deletion --------------
+	l.delete_node("D")
+	l.traverse()
+
+	l.delete_node("A")
+	l.traverse()
+
+	l.delete_node_at_position(1)
+	l.traverse()
